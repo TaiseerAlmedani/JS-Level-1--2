701f2478202544519e4980aef98763c9b925aba9
TAISEER ALMEDANI <taiseer.almedani@gmail.com>
2023-05-03 00:10:09 +0300

    init Commit

---
+++ Lesson 01/app.js
@@ -0,0 +1,30 @@
+console.log(123);
+console.log('Hi');
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,5]);
+// alert('this is an aleart')
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+console.error("this is an error");
+console.warn('this is a warning');
+console.time('time');
+  console.log('Hi');
+  console.log('Hi');
+  console.log('Hi');
+  console.log('Hi');
+  console.log('Hi');
+  console.log('Hi');
+  console.log('Hi');
+  console.timeEnd('time');
+  console.info('this is info')
+
+  //this is a comment
+  //!this is a comment
+  //?this is a comment
+
+  /* this 
+  is 
+  a muthiline 
+  comment */
