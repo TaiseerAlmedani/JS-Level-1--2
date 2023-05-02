3995004dda6ee37917cf8092c384f76d74858f76
TAISEER ALMEDANI <taiseer.almedani@gmail.com>
2023-05-03 00:34:05 +0300

    variables

---
+++ 02/app.js
@@ -0,0 +1,43 @@
+// var, let, const
+
+// var fname = 'Saro';
+// console.log(fname);
+// fname = 'taiseer'
+// console.log(fname);
+
+// letters, numbers, _ , $
+// var lastName= 'john'
+// console.log(lastName);
+
+// var lname;
+// lname = 'lname'
+// console.log(lname);
+
+// let name;
+// fname = 'saro'
+// console.log(fname);
+// let age;
+// console.log(age);
+// age = 79;
+// console.log(age);
+// age = 80;
+// console.log(age);
+
+
+// const age = 40;
+// console.log(age);
+// age = 41;
+
+// const person = {
+//   fname : 'saro',
+//   age : 40
+// }
+// console.log(person);
+// person.fname= 'sara';
+// console.log(person);
+
+const numbers = [1,2,3,4,7,9]
+console.log(numbers);
+numbers.push(11);
+console.log(numbers);
+numbers = [1,2,4,5,67,8]
